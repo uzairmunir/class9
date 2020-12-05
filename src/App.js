496ae5d74c0244eb54918bd295d96e7260dd7274
Uzair Munir <uzair8825@gmail.com>
2020-12-05 12:48:38 +0500

    Material Ui and Chart Js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import { Autorenew, CenterFocusStrong } from '@material-ui/icons';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import NavBar from './NavBar'
+import Chart from './Chart'
+import PieChart from './PieChart'
+import './App.css'
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    
+    },
+  
+  paper:{
+    marginTop:50,
+    width:400,
+    margin:'0 auto',
+    textAlign:'center',
+    padding:15,
+  },
+  input:{
+    width:'100%',
+    marginBottom:10,
+  }
+}));
+
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <h2>Material Ui</h2>
+      <NavBar/>
+      
+      <Paper elevation={3} className={classes.paper}>
+        <h1>Login</h1>
+        <form className={classes.root} noValidate autoComplete="off">
+         <TextField  label="Enter Email"className={classes.input} />
+         <TextField  label="Enter Password" className={classes.input}/>
+         <Button variant="contained" color="primary" className={classes.input}>
+       Submit
+      </Button>
+      </form>
+        </Paper><br/><hr/>
+        <h2 >Chart Js</h2>
+        <Chart/>
+        <PieChart/>
     </div>
   );
 }
